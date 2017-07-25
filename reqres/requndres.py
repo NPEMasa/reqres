aa5e8eae7aaf86a6be5e59deec869e81383f9a79
Masanobu Miyagi <masanobu.miyagi@lac.co.jp>
2017-07-26 07:41:53 +0900

    fixed reqres

--- reqres/requndres.py
+++ reqres/requndres.py
@@ -11,7 +11,7 @@ URL = sys.argv[param]
 
 class reqres():
     def msg(self):
-	help = """
+        help = """
 +--------------+
 | Command help |
 +--------------+
@@ -33,39 +33,38 @@ $ python requndres.py -u s http://example.com/
 $ python requndres.py -u i http://example.com/
 
 """
-	print help
+        print (help)
 
     def request(self):
-	headers = r.UA()
-	self.res = requests.get(URL, headers=headers)
-	self.res_txt = self.res.text
+        headers = self.UA()
+        self.res = requests.get(URL, headers=headers)
+        self.res_txt = self.res.text
 
     def res_state(self):
-	return self.res
+        return self.res
 
     def res_txt(self):
-	return self.res_txt
+        return self.res_txt
 
-    #URL = sys.argv[param]
     def UA(self):
-	if sys.argv[2] == "a6":
-	    self.headers = {'User-Agent': 'Mozilla/5.0 (Linux; U; Android 6.0.1; ja-jp; Nexus 6P) AppleWebKit/533.1 (KHTML, like Gecko) Version/6.0 Mobile Safari/533.1'}
-	elif sys.argv[2] == "a7":
-	    self.headers = {'User-Agent': 'Mozilla/5.0 (Linux; U; Android 7.0; ja-jp; Nexus 6P) AppleWebKit/533.1 (KHTML, like Gecko) Version/6.0 Mobile Safari/533.1'}
-	elif sys.argv[2] == "s":
-	    self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/8.0.8 Safari/600.8.9'}
-	elif sys.argv[2] == "c":
-	    self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36'}
-	elif sys.argv[2] == "i":
-	    self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; Trident/7.0; rv:11.0) like Gecko'}
-	elif sys.argv[2] == "f":
-	    self.headers = {'User-Agent': 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/20100101 Firefox/firefoxversion'}
+        if sys.argv[2] == "a6":
+            self.headers = {'User-Agent': 'Mozilla/5.0 (Linux; U; Android 6.0.1; ja-jp; Nexus 6P) AppleWebKit/533.1 (KHTML, like Gecko) Version/6.0 Mobile Safari/533.1'}
+        elif sys.argv[2] == "a7":
+            self.headers = {'User-Agent': 'Mozilla/5.0 (Linux; U; Android 7.0; ja-jp; Nexus 6P) AppleWebKit/533.1 (KHTML, like Gecko) Version/6.0 Mobile Safari/533.1'}
+        elif sys.argv[2] == "s":
+            self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/8.0.8 Safari/600.8.9'}
+        elif sys.argv[2] == "c":
+            self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36'}
+        elif sys.argv[2] == "i":
+            self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; Trident/7.0; rv:11.0) like Gecko'}
+        elif sys.argv[2] == "f":
+            self.headers = {'User-Agent': 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/20100101 Firefox/firefoxversion'}
 
 def main():
+    global r
+    r = reqres()
     if sys.argv[1] == "-u":
-        global r
-	r = reqres()
-        print "[*]Starting GET Request Process..."
+        print ("[*]Starting GET Request Process...")
         r.UA()
         r.request()
         print (r.res_txt.encode('utf-8'))
